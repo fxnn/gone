8bba8952c1b9ceade5646349c5e1533c6ed8a188
Felix Neumann <dev@fxnn.de>
2015-09-19 18:06:37 +0200

    Split basicFiler from Filer

--- filer/Filer.go
+++ filer/Filer.go
@@ -1,27 +1,19 @@
 package filer
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 // Maps incoming HTTP requests to the file system.
 type Filer struct {
-	err error
+	basicFiler
 }
 
 // Initializes a zeroe'd instance ready to use.
 func New() Filer {
 	return Filer{}
-}
-
-// Never forget to check for errors
-func (f *Filer) Err() error {
-	return f.err
 }
 
 // OpenReader opens a reader for the given request.
@@ -39,74 +31,5 @@ func (f *Filer) openReaderAtPath(path string) (reader io.ReadCloser) {
 		return nil
 	}
 	reader, f.err = os.Open(path)
-	return
-}
-
-func (f *Filer) assertPathInsideWorkingDirectory(path string) {
-	if f.err != nil {
-		return
-	}
-
-	var normalizedPath = f.normalizePath(path)
-	var wdPath = f.normalizePath(f.workingDirectory())
-
-	if f.err == nil && !strings.HasPrefix(normalizedPath, wdPath) {
-		f.err = NewPathNotFoundError(fmt.Sprintf("%s is not inside working directory", path))
-	}
-}
-
-func (f *Filer) normalizePath(path string) string {
-	if f.err != nil {
-		return path
-	}
-
-	var result string
-
-	result = f.absPath(path)
-
-	f.assertPathExists(result)
-	result = f.evalSymlinks(result)
-
-	// HINT: Remove .. and ., remove trailing slash
-	return f.cleanPath(result)
-}
-
-func (f *Filer) absPath(path string) (absPath string) {
-	if f.err != nil {
-		return path
-	}
-	absPath, f.err = filepath.Abs(path)
-	return
-}
-
-func (f *Filer) assertPathExists(path string) {
-	if f.err != nil {
-		return
-	}
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		f.err = NewPathNotFoundError(err.Error())
-	}
-}
-
-func (f *Filer) evalSymlinks(path string) (hardPath string) {
-	if f.err != nil {
-		return path
-	}
-	hardPath, f.err = filepath.EvalSymlinks(path)
-	return
-}
-
-func (f *Filer) cleanPath(path string) string {
-	if f.err != nil {
-		return path
-	}
-	return filepath.Clean(path)
-}
-
-func (f *Filer) workingDirectory() (wd string) {
-	if f.err != nil {
-		return ""
-	}
-	wd, f.err = os.Getwd()
 	return
 }
---
+++ filer/basicFiler.go
@@ -0,0 +1,89 @@
+package filer
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// The basicFiler implements basic algorithms and error handling needed when
+// dealing with files.
+// It basically wraps golang library functions for error handling.
+type basicFiler struct {
+	err error
+}
+
+// Never forget to check for errors
+func (f *basicFiler) Err() error {
+	return f.err
+}
+
+func (f *basicFiler) assertPathInsideWorkingDirectory(path string) {
+	if f.err != nil {
+		return
+	}
+
+	var normalizedPath = f.normalizePath(path)
+	var wdPath = f.normalizePath(f.workingDirectory())
+
+	if f.err == nil && !strings.HasPrefix(normalizedPath, wdPath) {
+		f.err = NewPathNotFoundError(fmt.Sprintf("%s is not inside working directory", path))
+	}
+}
+
+func (f *basicFiler) normalizePath(path string) string {
+	if f.err != nil {
+		return path
+	}
+
+	var result string
+
+	result = f.absPath(path)
+
+	f.assertPathExists(result)
+	result = f.evalSymlinks(result)
+
+	// HINT: Remove .. and ., remove trailing slash
+	return f.cleanPath(result)
+}
+
+func (f *basicFiler) absPath(path string) (absPath string) {
+	if f.err != nil {
+		return path
+	}
+	absPath, f.err = filepath.Abs(path)
+	return
+}
+
+func (f *basicFiler) assertPathExists(path string) {
+	if f.err != nil {
+		return
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		f.err = NewPathNotFoundError(err.Error())
+	}
+}
+
+func (f *basicFiler) evalSymlinks(path string) (hardPath string) {
+	if f.err != nil {
+		return path
+	}
+	hardPath, f.err = filepath.EvalSymlinks(path)
+	return
+}
+
+func (f *basicFiler) cleanPath(path string) string {
+	if f.err != nil {
+		return path
+	}
+	return filepath.Clean(path)
+}
+
+func (f *basicFiler) workingDirectory() (wd string) {
+	if f.err != nil {
+		return ""
+	}
+	wd, f.err = os.Getwd()
+	return
+}
