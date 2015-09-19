0ba9a529be8fa18231d3416a402c3177d6951501
Felix Neumann <dev@fxnn.de>
2015-09-19 18:11:22 +0200

    Update filer doc.go

--- filer/doc.go
+++ filer/doc.go
@@ -1,4 +1,11 @@
-// The `filer` package implements the wikis link to the filesystem.
-// It implements reading and writing pages, and provides a HTTP handler which
-// simply delivers the raw file content.
+// The filer package implements the wikis link to the filesystem.
+// It provides basic file access and therefore implements error handling and
+// serves just the right abstractions.
+//
+// It is important to note, that the error handling is implemented using a
+// saved error value.
+// As soon as an error occured, all functions get no-ops.
+// This way, implementation gets a lot easier and more readable.
+// However, callers and developers always have to ensure to correctly check
+// for errors, as the type system won't ensure this anymore.
 package filer
