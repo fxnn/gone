cf0ae7a7be43f2eac4996c0234e8cd2209f0e2ac
Felix Neumann <dev@fxnn.de>
2016-01-13 18:14:08 +0100

    wip #19: documentation

--- authenticator/bruteblocker/doc.go
+++ authenticator/bruteblocker/doc.go
@@ -1,3 +1,13 @@
-// Package bruteblocker is a service to block brute force attacks on the
-// authentication mechanism.
+// Package bruteblocker allows to slow down bruteforce attacks by delaying
+// request responses or blocking further authentication attempts for some
+// time.
+// This makes it harder for the attacker to guess passwords or user names,
+// while retaining an acceptable amount of usability for normal users.
+//
+// The idea is to keep track of the number of failed authentication attempts
+// as well as the timestamp of the last authentication attempt.
+// This is done per user, per IP address and globally.
+//
+// Per failed authentication attempt, the delay is increased.
+// After a while, the delay is reset to zero.
 package bruteblocker
