1080751245410abb158acb13bd0dd17e8513fc9e
Felix Neumann <dev@fxnn.de>
2016-01-17 20:53:18 +0100

    logger: print response size and status code

--- http/logger.go
+++ http/logger.go
@@ -10,10 +10,11 @@ import (
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var start = time.Now()
+		var stats = &responseWriterWithStats{wrapped: w}
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(stats, r)
 
-		log.Printf("[%s] %s %s [%s]", clientIP(r), r.Method, r.URL, time.Since(start))
+		log.Printf("[%s] %d %s %s [%d bytes in %s]", clientIP(r), stats.status, r.Method, r.URL, stats.bytesWritten, time.Since(start))
 	})
 }
 
@@ -24,4 +25,28 @@ func clientIP(r *http.Request) string {
 		return strings.Join(values, ", ")
 	}
 	return r.RemoteAddr
+}
+
+type responseWriterWithStats struct {
+	wrapped      http.ResponseWriter
+	status       int
+	bytesWritten int
+}
+
+func (w *responseWriterWithStats) Header() http.Header {
+	return w.wrapped.Header()
+}
+
+func (w *responseWriterWithStats) Write(content []byte) (written int, err error) {
+	if w.status == 0 {
+		w.status = http.StatusOK
+	}
+	written, err = w.wrapped.Write(content)
+	w.bytesWritten += written
+	return
+}
+
+func (w *responseWriterWithStats) WriteHeader(status int) {
+	w.wrapped.WriteHeader(status)
+	w.status = status
 }
