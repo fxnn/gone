7f8b9f915c19cc097c5d7bee7cf6bb47a3517b1c
Felix Neumann <dev@fxnn.de>
2016-08-28 15:28:37 +0200

    filestore: builds on windows

--- store/filestore/pathio_windows.go
+++ store/filestore/pathio_windows.go
@@ -1,8 +1,23 @@
 package filestore
 
 import "github.com/fxnn/gopath"
+import "os"
+import "math/rand"
+import "strconv"
 
 func isPathWriteable(p gopath.GoPath) bool {
-	// HINT: unclear wether we got to implement something here
-	return true
+	var nonExistantFile = p.JoinPath("githubcom-fxnn-gone")
+	for !nonExistantFile.HasErr() && nonExistantFile.IsExists() {
+		nonExistantFile = nonExistantFile.Append("-" + strconv.Itoa(rand.Int()))
+	}
+	if nonExistantFile.HasErr() {
+		return false
+	}
+
+	var closer, err = os.Create(nonExistantFile.Path())
+	if closer != nil {
+		closer.Close()
+	}
+
+	return err == nil
 }
